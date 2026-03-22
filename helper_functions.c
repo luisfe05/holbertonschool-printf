65b5ab793ce6d37ffefb45969c0aebad885602ae
luisfe05 <lgonzalez05@gmail.com>
2026-03-21 20:03:19 -0400

    upload project

--- helper_functions.c
+++ helper_functions.c
@@ -7,49 +7,19 @@
  */
 int print_char(va_list args)
 {    char c;
-
-    c = va_arg(args, int); /*get the character from the arguments*/
-    write(1, &c, 1); /*print the character*/
-    return (1); /*return 1 because it printed one character*/
-}
-
-/**
- * print_string - print a string
- * @args: va_list of arguments
- * return: number of characters printed
- */
-int print_string(va_list args)
-{    char *str;
+    int num;
     int count;
-
-    str = va_arg(args, char *); /*get the string from the arguments*/
-    if (str == NULL)
-        str = "(null)"; /*if the string is NULL, print (null)*/
-    count = 0;
-    while (*str) /*go across all letter up to 0*/
-    {        write(1, str, 1); /*print the letter*/
-        count++; /*sum 1*/
-        str++;
-    }
-    return (count); /*return the number of printed characters*/
-}
-
-/**
- * print_int - print an integer
- * @args: va_list of arguments
- * return: number of characters printed
- */
-int print_int(va_list args)
-{    int num;
-    int count;
-    char buffer[12]; /*buffer to hold the string representation of the integer*/
+    char *buffer;
     int i;
 
     num = va_arg(args, int); /*get the integer from the arguments*/
     if (num == 0)
     {        write(1, "0", 1); /*if the integer is 0, print 0*/
-        return (1); /*return 1 because we printed one character*/
+        return (1); 
     }
+    buffer = malloc(12 * sizeof(char)); /*allocate memory for the buffer*/
+    if (buffer == NULL)
+        return (-1); /*return -1 if malloc error*/
     count = 0;
     if (num < 0)
     {        write(1, "-", 1); /*if the integer is negative, print '-'*/
@@ -63,7 +33,8 @@ int print_int(va_list args)
     }
     while (i > 0)
     {        write(1, &buffer[--i], 1); /*print the digits in reverse order*/
-        count++; /*sum 1*/
+        count++; 
     }
+    free(buffer); /*free the allocated memory*/
     return (count); /*return the number of printed characters*/
 }
