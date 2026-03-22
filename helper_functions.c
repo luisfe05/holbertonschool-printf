3465cd8f55a7ab1545f67eeda928ae36dcbaeca5
luisfe05 <lgonzalez05@gmail.com>
2026-03-21 20:07:15 -0400

    upload project

--- helper_functions.c
+++ helper_functions.c
@@ -1,40 +1,82 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * print_char - pritnt a single character
+ * print_char - print a single character
  * @args: va_list of arguments
- * return: number of characters printed
+ * Return: number of characters printed
  */
 int print_char(va_list args)
-{    char c;
-    int num;
-    int count;
-    char *buffer;
-    int i;
+{
+	char c;
 
-    num = va_arg(args, int); /*get the integer from the arguments*/
-    if (num == 0)
-    {        write(1, "0", 1); /*if the integer is 0, print 0*/
-        return (1); 
-    }
-    buffer = malloc(12 * sizeof(char)); /*allocate memory for the buffer*/
-    if (buffer == NULL)
-        return (-1); /*return -1 if malloc error*/
-    count = 0;
-    if (num < 0)
-    {        write(1, "-", 1); /*if the integer is negative, print '-'*/
-        count++; /*sum 1*/
-        num = -num; /*make the integer positive*/
-    }
-    i = 0;
-    while (num > 0)
-    {        buffer[i++] = (num % 10) + '0'; /*get the last digit and convert it to a character*/
-        num /= 10; /*remove the last digit*/
-    }
-    while (i > 0)
-    {        write(1, &buffer[--i], 1); /*print the digits in reverse order*/
-        count++; 
-    }
-    free(buffer); /*free the allocated memory*/
-    return (count); /*return the number of printed characters*/
+	c = va_arg(args, int);
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * print_string - print a string
+ * @args: va_list of arguments
+ * Return: number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *str;
+	int count;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		str = "(null)";
+	count = 0;
+	while (*str)
+	{
+		write(1, str, 1);
+		count++;
+		str++;
+	}
+	return (count);
+}
+
+/**
+ * print_int - print an integer
+ * @args: va_list of arguments
+ * Return: number of characters printed
+ */
+int print_int(va_list args)
+{
+	int num;
+	int count;
+	char *buffer;
+	int i;
+
+	num = va_arg(args, int);
+	if (num == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	buffer = malloc(12 * sizeof(char));
+	if (buffer == NULL)
+		return (-1);
+	count = 0;
+	if (num < 0)
+	{
+		write(1, "-", 1);
+		count++;
+		num = -num;
+	}
+	i = 0;
+	while (num > 0)
+	{
+		buffer[i++] = (num % 10) + '0';
+		num /= 10;
+	}
+	while (i > 0)
+	{
+		write(1, &buffer[--i], 1);
+		count++;
+	}
+	free(buffer);
+	return (count);
 }
